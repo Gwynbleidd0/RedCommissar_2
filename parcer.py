739372cd7c0891eff19dfb2651a47bf9ffbf5957
Gwynbleidd0 <gwinbleidd12@gmail.com>
2018-11-26 15:24:33 +0300

    121

--- parcer.py
+++ parcer.py
@@ -1,26 +1,33 @@
 from requests_html import HTMLSession
 import fille 
+#from parsel import Selector
+#from urllib.parse import urljoin
+#import json
+#import fille
 def get_url():
     ses=HTMLSession()
     r = ses.get('https://s11028.edu35.ru/2013-06-12-15-17-31/raspisanie')
     i=0
-    m=len(r.html.find('.at_url'))
+    m=len(r.html.find('.at_icon'))
     hg = []
+#date=fille.get_real_date()
+#date= date +
     while i<m:
-        g=1
         f=r.html.find('.at_icon')[i].attrs
         f=f.get('href')
         print(f)
-        listhor=['1 смена','1_смена','1смена','1 СМЕНА']
-        for j in listhor:
-            result=f.find(j)
-#        fh=f.find('224/')
-#        day_real=fille.get_real_date()
-#        day=int(f[fh+4:fh+6])
-#        if result!=-1 and day_real==day:
-        if result!=-1: 
+        result=f.find('1 смена')
+        if result!=-1:
             hg.append(f)
-
+        else:
+            result=f.find('1_смена')
+            if result!=-1:
+                hg.append(f)
+            else:
+                result=f.find('1смена')
+#    while g<len(f.get('href'):
+#    print(result)
+#    hg.append(f.get('href'))
         i=i+1
     month=fille.get_month()
     try:
@@ -28,10 +35,14 @@ def get_url():
     except IndexError:
         return(0,0)
     kh=uk.find(month)
-    if kh == -1:
-        month=month.upper()
-        kh=uk.find(month)
     fh=uk.find('224/')
     cast = uk[fh+4:kh+len(month)]
     return(hg[-1],cast)
+#print(get_url())
+#print(get_url())
 
+
+#print(Selector(r.text).css('.at_icon').extract())
+#Links = r.html.absolute_links
+#for i in Links:
+#    print(i)
